e15ebaa102b67ed92157e1573060ba979b5dfe93
Devil-Aura <imh203356@gmail.com>
2026-05-04 20:25:14 +0530

    fix: DB_NAME space causes InvalidName error in MongoDB

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 API_ID = 22768311
 API_HASH = "702d8884f48b42e865425391432b3794"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8402120935:AAHt7K01WwoGzdwxyP1-5ANzOkvyv4U97Cs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 OWNER_ID = 6040503076
 DB_URL = "mongodb+srv://control:control0123@cluster0.ffqebqy.mongodb.net/?appName=Cluster0"
-DB_NAME = "thumbnail bot"
+DB_NAME = "thumbnail_bot"
 START_PIC = "https://i.ibb.co/zV9rq4Jf/x.jpg"
