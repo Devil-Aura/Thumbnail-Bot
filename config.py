7ac0876ba11001c7cd19945e00f8dbd6fc505cd1
Devil-Aura <imh203356@gmail.com>
2026-05-07 20:19:56 +0530

    feat: add TMDB and FANART keys

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
-API_ID = 22768311
-API_HASH = "702d8884f48b42e865425391432b3794"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_ID = 6040503076
-DB_URL = "mongodb+srv://control:control0123@cluster0.ffqebqy.mongodb.net/?appName=Cluster0"
-DB_NAME = "thumbnail_bot"
-START_PIC = "https://i.ibb.co/zV9rq4Jf/x.jpg"
+API_ID          = 22768311
+API_HASH        = "702d8884f48b42e865425391432b3794"
+BOT_TOKEN       = os.environ.get("BOT_TOKEN", "")
+OWNER_ID        = 6040503076
+DB_URL          = "mongodb+srv://control:control0123@cluster0.ffqebqy.mongodb.net/?appName=Cluster0"
+DB_NAME         = "thumbnail_bot"
+START_PIC       = "https://i.ibb.co/zV9rq4Jf/x.jpg"
+TMDB_API_KEY    = os.getenv("TMDB_API_KEY", "a61ea99de574d69d9793cc254d07fa32")
+FANART_TV_KEY   = os.getenv("FANART_TV_KEY", "84823d47de8e375735375558b5c0ab86")
