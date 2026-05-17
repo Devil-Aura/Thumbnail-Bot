a08fce368fef4a3972cd89715825a7cf1f78f5ed
Devil-Aura <imh203356@gmail.com>
2026-05-17 10:54:01 +0530

    feat: add GITHUB_PAT config var for /update_repo authenticated cloning

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ DB_NAME         = "thumbnail_bot"
 START_PIC       = "https://i.ibb.co/zV9rq4Jf/x.jpg"
 TMDB_API_KEY    = os.getenv("TMDB_API_KEY", "a61ea99de574d69d9793cc254d07fa32")
 FANART_TV_KEY   = os.getenv("FANART_TV_KEY", "84823d47de8e375735375558b5c0ab86")
+GITHUB_PAT      = os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN", "")
